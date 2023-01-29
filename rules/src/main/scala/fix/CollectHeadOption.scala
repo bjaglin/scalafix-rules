cb29f3fd8057c65bef0f02e679a6502ba55e54ef
Brice Jaglin <bjaglin@gmail.com>
2023-01-29 15:31:49 +0100

    unapply -> type matching child

--- rules/src/main/scala/fix/CollectHeadOption.scala
+++ rules/src/main/scala/fix/CollectHeadOption.scala
@@ -8,14 +8,17 @@ import scala.meta.Term
 class CollectHeadOption extends SyntacticRule("CollectHeadOption") {
   override def fix(implicit doc: SyntacticDocument): Patch = {
     doc.tree.collect {
-      case t @ Term.Select(
-            Term.Apply(
-              Term.Select(obj, Term.Name("collect")),
-              func :: Nil
-            ),
-            Term.Name("headOption")
-          ) =>
-        Patch.replaceTree(t, s"${obj}.collectFirst${func}")
+      case s: Term.Select if s.name.value == "collect" && ((s.parent, s.parent.flatMap(_.parent)) match {
+            case (Some(a: Term.Apply), Some(s0: Term.Select))
+                if a.argClause.values.length == 1 && s0.name.value == "headOption" =>
+              true
+            case _ => false
+          }) =>
+        val func = s.parent match {
+          case Some(a: Term.Apply) => a.argClause.values.head
+          case _ => ???
+        }
+        Patch.replaceTree(s.parent.get.parent.get, s"${s.qual}.collectFirst${func}")
     }
   }.asPatch
 }
